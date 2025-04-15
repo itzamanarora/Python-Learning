077f3855cafb69bea6e0a29d8c6bf2838c89b174
Aman Arora <amanarora99005@gmail.com>
2025-04-15 21:57:33 +0530

    My Python Learning

---
+++ Variables/Variables.py
@@ -0,0 +1,25 @@
+a = 10
+b = 2.12
+c = "STRING"
+
+# Print the Values 
+print(a)
+print(b)
+print(c)
+
+# Print the variable Types
+print(type(a))
+print(type(b))
+print(type(c))
+
+# Many Values to Mutiple Variables
+d, e, f = "First", "Second", "Third"
+
+# Print the Values
+print(d,e,f)
+
+# One Value to Mutiple Variables
+g = h = i = "Mutiple Values"
+
+# Print the values
+print(g,h,i)
